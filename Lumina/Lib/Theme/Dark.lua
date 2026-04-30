9d7b6559225b41cfa8e5ea5ad221f04462c871e3
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-29 22:46:02 -0300

    Create Dark.lua

---
+++ Lumina/Lib/Theme/Dark.lua
@@ -0,0 +1,12 @@
+local Dark = {
+    Main = Color3.fromRGB(25, 25, 25),
+    Secondary = Color3.fromRGB(35, 35, 35),
+    Accent = Color3.fromRGB(60, 60, 60),
+    Outline = Color3.fromRGB(45, 45, 45),
+    Text = Color3.fromRGB(240, 240, 240),
+    SubText = Color3.fromRGB(150, 150, 150),
+    Section = Color3.fromRGB(30, 30, 30),
+    Tbar = Color3.fromRGB(20, 20, 20)
+}
+
+return Dark
