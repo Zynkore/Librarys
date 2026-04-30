0d0e8ddabfa91acaf6a7800dd69df4e08f7da9c0
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-29 22:45:35 -0300

    Create Light.lua

---
+++ Lumina/Lib/Theme/Light.lua
@@ -0,0 +1,12 @@
+local Light = {
+    Main = Color3.fromRGB(240, 240, 240),
+    Secondary = Color3.fromRGB(225, 225, 225),
+    Accent = Color3.fromRGB(0, 120, 215),
+    Outline = Color3.fromRGB(200, 200, 200),
+    Text = Color3.fromRGB(20, 20, 20),
+    SubText = Color3.fromRGB(80, 80, 80),
+    Section = Color3.fromRGB(230, 230, 230),
+    Tbar = Color3.fromRGB(255, 255, 255)
+}
+
+return Light
