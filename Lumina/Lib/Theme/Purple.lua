ebd1777d3c1de5fdaffd5a5fb909745cdf22ff47
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-29 22:43:08 -0300

    Create Purple.lua

---
+++ Lumina/Lib/Theme/Purple.lua
@@ -0,0 +1,12 @@
+local Purple = {
+    Main = Color3.fromRGB(20, 15, 25),
+    Secondary = Color3.fromRGB(30, 20, 40),
+    Accent = Color3.fromRGB(140, 80, 250),
+    Outline = Color3.fromRGB(60, 40, 80),
+    Text = Color3.fromRGB(255, 255, 255),
+    SubText = Color3.fromRGB(200, 170, 240),
+    Section = Color3.fromRGB(35, 25, 50),
+    Tbar = Color3.fromRGB(15, 10, 20)
+}
+
+return Purple
