e566ab627ae36eaf600a894ded3821d7fc88da85
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-29 22:44:40 -0300

    Create Green.lua

---
+++ Lumina/Lib/Theme/Green.lua
@@ -0,0 +1,12 @@
+local Green = {
+    Main = Color3.fromRGB(10, 20, 10),
+    Secondary = Color3.fromRGB(15, 30, 15),
+    Accent = Color3.fromRGB(50, 255, 50),
+    Outline = Color3.fromRGB(25, 60, 25),
+    Text = Color3.fromRGB(255, 255, 255),
+    SubText = Color3.fromRGB(150, 220, 150),
+    Section = Color3.fromRGB(20, 40, 20),
+    Tbar = Color3.fromRGB(5, 15, 5)
+}
+
+return Green
