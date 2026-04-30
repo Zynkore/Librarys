a384301418ede73b0f8903cee449d38cf6f81114
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-29 23:28:06 -0300

    Update Label.lua

--- Lumina/Lib/Element/Label.lua
+++ Lumina/Lib/Element/Label.lua
@@ -1,27 +1,17 @@
-local Label = {}
+local New = loadstring(game:HttpGet("https://raw.githubusercontent.com/MarkhubOfc/Librarys/main/Lumina/Lib/Utils/New.lua"))()
 
-function Label.new(parent, data)
-    local Theme = parent.Theme
-    local Target = parent.Container or parent.Page
-
-    local Frame = Instance.new("Frame")
-    Frame.Name = "Label_" .. data.Name
-    Frame.Size = UDim2.new(1, 0, 0, 25)
-    Frame.BackgroundTransparency = 1
-    Frame.Parent = Target
-
-    local Text = Instance.new("TextLabel")
-    Text.Size = UDim2.new(1, -10, 1, 0)
-    Text.Position = UDim2.new(0, 10, 0, 0)
-    Text.BackgroundTransparency = 1
-    Text.Text = data.Name
-    Text.TextColor3 = Theme.Text
-    Text.TextSize = 12
-    Text.Font = Enum.Font.Gotham
-    Text.TextXAlignment = Enum.TextXAlignment.Left
-    Text.Parent = Frame
-
-    return Frame
-end
-
-return Label
+return {
+    new = function(parent, data)
+        local Target = parent.Container or parent.Page
+        return New("TextLabel", {
+            Parent = Target,
+            Size = UDim2.new(1, 0, 0, 20),
+            BackgroundTransparency = 1,
+            Text = data.Name,
+            TextColor3 = parent.Theme.Text,
+            Font = Enum.Font.Gotham,
+            TextSize = 12,
+            TextXAlignment = Enum.TextXAlignment.Left
+        })
+    end
+}
