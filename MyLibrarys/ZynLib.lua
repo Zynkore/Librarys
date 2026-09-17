782fc9855577b556ab9c463f33c82497300ec0b5
Eu sou brasileiro <zynkore.ofc@gmail.com>
2026-09-17 07:59:26 -0300

    Implement minimize and close buttons in MakeWindow
    Added functionality for minimize and close buttons in the window.

--- MyLibrarys/ZynLib.lua
+++ MyLibrarys/ZynLib.lua
@@ -6,6 +6,7 @@ local WS = GeneralModule:Service("Workspace")
 local CoreGui = (gethui and gethui() or GeneralModule:Service("CoreGui"))
 function Library:MakeWindow(Config)
   local Title = Config.Title or Config.Name or "Missing title"
+  local StartMinimized = Config.StartMinimized or Config.startMinimized or Config.startminimized or Config.sm or Config.Sm or Config.SM or Config.sM or false
   local UICorner = Config.UICorner or Config.UiCorner or Config.uiCorner or Config.uicorner or false
   local CornerRadius = Config.CornerRadius or Config.cornerRadius or Config.cornerradius or UDim.new(0, 6)
   for _, Obj in pairs(CoreGui:GetChildren()) do
@@ -56,7 +57,69 @@ function Library:MakeWindow(Config)
       Parent = Window
     })
   end
+  
+  local TitleBar = GeneralModule:new("Frame", {
+    Name = GenerateRandomName(999),
+    Size = UDim2.new(0, Window.AbsoluteSize.X, 0, 35),
+    BackgroundColor3 = Color3.fromRGB(8, 9, 12),
+    BorderColor3 = Color3.fromRGB(9, 59, 164),
+    Parent = Window
+  })
+  if UICorner == true then
+    GeneralModule:new("UICorner", {
+      Name = GenerateRandomName(999),
+      BottomLeftRadius = UDim.new(0, 0),
+      BottomRightRadius = UDim.new(0, 0),
+      TopLeftRadius = CornerRadius,
+      TopRightRadius = CornerRadius,
+      Parent = TitleBar
+    })
+    GeneralModule:new("UIStroke", {
+      Name = GenerateRandomName(999),
+      Color = Color3.fromRGB(9, 59, 164),
+      Thickness = 3,
+      Parent = TitleBar
+    })
+  end
+  
+  local MinimizeButtonSizeY = 0
+  local IsWindowMinimized = StartMinimized
+  local MinimizeButton = GeneralModule:new("ImageButton", {
+    Name = GenerateRandomName(999),
+    Position = UDim2.new(0, TitleBar.AbsoluteSize.X - 53, 0, 1.25),
+    Size = UDim2.new(0, 30, 0, TitleBar.AbsoluteSize.Y - 3),
+    Image = "rbxassetid://10747384394",
+    ImageColor3 = Color3.fromRGB(255, 255, 255),
+    Parent = TitleBar
+  })
+  MinimizeButton.MouseButton1Click:Connect(function()
+    IsWindowMinimized = not IsWindowMinimized
+    if IsWindowMinimized then
+      MinimizeButtonSizeY = tonumber(Window.AbsoluteSize.Y)
+      Window.Size = UDim2.new(0, Window.AbsoluteSize.X, 0, TitleBar.AbsoluteSize.Y)
+    else
+      Window.Size = UDim2.new(0, Window.AbsoluteSize.X, 0, MinimizeButtonSizeY)
+    end
+  end)
+  if IsWindowMinimized then
+    MinimizeButtonSizeY = tonumber(Window.AbsoluteSize.Y)
+    Window.Size = UDim2.new(0, Window.AbsoluteSize.X, 0, MinimizeButtonSizeY)
+  end
+  
+  local CloseButton = GeneralModule:new("ImageButton", {
+    Name = GenerateRandomName(999),
+    Position = UDim2.new(0, TitleBar.AbsoluteSize.X - 38, 0, 1.25),
+    Size = UDim2.new(0, 30, 0, TitleBar.AbsoluteSize.Y - 3),
+    Image = "rbxassetid://10747384394",
+    ImageColor3 = Color3.fromRGB(255, 255, 255),
+    Parent = TitleBar
+  })
 end
 
-
+Library:MakeWindow({
+  Title = "Window title",
+  StartMinimized = false,
+  UICorner = true,
+  CornerRadius = UDim.new(0, 6)
+})
 return Library
